db932a83f7f1830c7e52581d0f122a86fd733a16
axlfrost <ale.gonzalezlevio@gmail.com>
2020-07-19 19:26:01 -0400

    Corrección especie pokémon

--- src/app/pipes/flavorText-to-Spanish.pipe.ts
+++ src/app/pipes/flavorText-to-Spanish.pipe.ts
@@ -5,17 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FlavorTextToSpanish implements PipeTransform {
 
-  transform(value: any[]): string {
-    return this.getTranslatedName(value);
+  transform(value: any[], type?: string): string {
+    return this.getTranslatedName(value, type);
   }
 
-  public getTranslatedName(list: any[]) {
-    for (const value of list) {
-      if (value.language.name === 'es') {
-        return value.flavor_text;
+  public getTranslatedName(list: any[], type: string) {
+    if( type == 'genus') {
+      return list.filter(obj => obj.language.name == 'es')[0].genus;
+    } else {
+      for (const value of list) {
+        if (value.language.name === 'es') {
+          return value.flavor_text;
+        }
       }
     }
   }
-
 
 }
